0df7bd03a672eaa79b87f77466f187c7c54d6f8e
JuaniniGC <juangarciacarballo@gmail.com>
2025-03-24 11:14:15 +0100

    feat: add get años amortizados funcion

--- src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionAllianzImpl.java
+++ src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionAllianzImpl.java
@@ -1,4 +1,16 @@
 package com.babel.helloworld.gestionSiniestros.service;
 
-public class DepreciacionAllianzImpl {
+import com.babel.helloworld.gestionSiniestros.model.Bien;
+
+import java.time.LocalDate;
+
+public class DepreciacionAllianzImpl implements Depreciacion{
+    @Override
+    public Double CalcularPrecioBien(LocalDate fechaSiniestro, Bien bien) {
+        Double valoracion = bien.valorCompra();
+        for(int anyo = 0; anyo<=bien.nombre().getTiempoAmortizacion(); anyo++){
+
+        }
+        return valoracion + valoracion*0.05;
+    }
 }
