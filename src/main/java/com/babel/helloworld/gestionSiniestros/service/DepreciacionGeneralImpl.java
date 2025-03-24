c31abcc8106070b244017bb4abeea57a1b890a05
laurahuertes <laura.huertes@babelgroup.com>
2025-03-24 11:21:40 +0100

    Añadida funcionalidad a Bien

--- src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionGeneralImpl.java
+++ src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionGeneralImpl.java
@@ -5,9 +5,12 @@ import com.babel.helloworld.gestionSiniestros.model.Bien;
 import java.time.LocalDate;
 
 public class DepreciacionGeneralImpl implements Depreciacion {
+    private Double valorResidual;
+
     @Override
     public Double CalcularPrecioBien(LocalDate fechaSiniestro, Bien bien) {
-
+        int tiempoAmortizacion = bien.nombre().getTiempoAmortizacion();
+        double valorCompra = bien.valorCompra();
 
         return 0.0;
     }
