f9aefa87d470f7ff570a329fce29e7fba59a7fab
Marcos Morales Márquez <marcos.morales@babelgroup.com>
2025-03-24 11:37:45 +0100

    Depreciacion Mapfre

--- src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionMapfreImpl.java
+++ src/main/java/com/babel/helloworld/gestionSiniestros/service/DepreciacionMapfreImpl.java
@@ -1,4 +1,36 @@
 package com.babel.helloworld.gestionSiniestros.service;
 
-public class DepreciacionMapfreImpl {
+import com.babel.helloworld.gestionSiniestros.model.Bien;
+import java.time.LocalDate;
+
+public class DepreciacionMapfreImpl implements Depreciacion
+{
+    private final double valorResidual = 0.17;
+
+    @Override
+    public Double CalcularPrecioBien(LocalDate fechaSiniestro, Bien bien)
+    {
+        /*
+        -La depreciación no es acumulada.
+        Por tanto, en el ejemplo anterior el cálculo sería: 1500 - (1500*0.2) = 1200 € -> Valor real
+        -Aplica un 17% de valor residual
+         */
+
+        int anyosCompra = bien.getAnyosAmortizados(fechaSiniestro).getYears();
+
+        int tiempoAmortizacion = bien.nombre().getTiempoAmortizacion();
+
+        double porcentajeDepreciacion = (double) 1 / tiempoAmortizacion;
+
+        double valor = bien.valorCompra() * (1 - porcentajeDepreciacion * anyosCompra);
+
+
+        double valorMinimo = bien.valorCompra() * valorResidual;
+        if (valor < valorMinimo)
+        {
+            valor = valorMinimo;
+        }
+
+        return valor;
+    }
 }
