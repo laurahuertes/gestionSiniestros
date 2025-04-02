61ca6eb6d8495b47b917f1d2bf7cf347f3d169af
JuanGCarballo <juan.gcarballo@babelgroup.com>
2025-04-02 10:04:24 +0200

    feat: enhance valoracion implementation with depreciacion strategy

--- src/main/java/com/babel/helloworld/gestionSiniestros/service/valoracion/ValoracionImpl.java
+++ src/main/java/com/babel/helloworld/gestionSiniestros/service/valoracion/ValoracionImpl.java
@@ -1,16 +1,28 @@
 package com.babel.helloworld.gestionSiniestros.service.valoracion;
 
+import com.babel.helloworld.gestionSiniestros.model.Bien.Bien;
 import com.babel.helloworld.gestionSiniestros.model.Siniestro.Siniestro;
+import com.babel.helloworld.gestionSiniestros.service.depreciacion.Depreciacion;
+import com.babel.helloworld.gestionSiniestros.service.depreciacion.DepreciacionStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ValoracionImpl implements Valoracion{
 
+    private DepreciacionStrategy depreciacionStrategy;
 
+    public ValoracionImpl(DepreciacionStrategy depreciacionStrategy) {
+        this.depreciacionStrategy = depreciacionStrategy;
+    }
 
     @Override
     public Double calcularValoracionBienesAfectados(Siniestro siniestro) {
-
-        return 0.0;
+        Depreciacion depreciacion = depreciacionStrategy.getDepreciacion(siniestro.getAseguradora());
+        Double res = 0.0;
+        for (Bien bien: siniestro.getBienesAfectados()) {
+            res += depreciacion.CalcularPrecioBien(siniestro.getFechaOcurrencia(), bien);
+        }
+        return res;
     }
+
 }
